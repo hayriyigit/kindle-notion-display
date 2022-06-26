c9af3b4f0b52ec44ef6bbf84944ff154fcbc6bfa
hayriyigit <hayriyigit@protonmail.com>
2022-06-26 17:12:39 +0300

    fix date

--- public/scripts/clock.js
+++ public/scripts/clock.js
@@ -3,7 +3,7 @@ var day = document.getElementById('day')
 var day_name = document.getElementById('day_name')
 
 var d1 = new Date().toLocaleString('en-US', { timeZone: 'Europe/Istanbul' });;
-
+d1 = new Date(d1)
 time.innerHTML = d1.getHours() + ':' + d1.getMinutes()
 day.innerHTML = d1.getDate()
 day_name.innerHTML = d1.toLocaleString('tr', {weekday:'short'})
