7c6b637e5696bf2853039dc64883d2d48814870a
hayriyigit <hayriyigit@protonmail.com>
2022-06-26 15:27:34 +0300

    update for vercel

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/snap', async (req, res) => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.setViewport({ width: 600, height: 800 });``
-  await page.goto(process.env.SCREENSHOT_URL || 'localhost:3000', { waitUntil: ['networkidle0'] });
+  await page.goto(process.env.SCREENSHOT_URL || 'http://localhost:3000', { waitUntil: ['networkidle0'] });
   await page.screenshot({
     path: '/tmp/screenshot.png',
   });
